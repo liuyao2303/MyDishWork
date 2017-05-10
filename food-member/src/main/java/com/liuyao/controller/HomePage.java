d15d56270dbfe02714d384c0cd815cb8c4712a4f
xiaoliu <18252063065@163.com>
2017-05-10 17:39:09 +0800

    project structure add

---
+++ food-member/src/main/java/com/liuyao/controller/HomePage.java
@@ -0,0 +1,21 @@
+package com.liuyao.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Created by xiaoliu on 2017/5/10.
+ */
+@Controller
+@RequestMapping(name = "/")
+public class HomePage {
+
+    @RequestMapping("index")
+    public ModelAndView home() {
+
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("home");
+        return mv;
+    }
+}
